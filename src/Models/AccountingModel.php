c8e9a4478f8a2e3c833290dbad2610f1a3716315
Ali Raza Marchal <kh.marchal@gmail.com>
2026-06-04 12:39:54 +0500

    fix: auto-prefix all accounting model tables with accounting_

--- src/Models/AccountingModel.php
+++ src/Models/AccountingModel.php
@@ -5,6 +5,7 @@ namespace Alimarchal\LaravelChartOfAccounts\Models;
 use Alimarchal\LaravelChartOfAccounts\Concerns\HasUserTracking;
 use Alimarchal\LaravelChartOfAccounts\Concerns\LogsAccountingActivity;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 abstract class AccountingModel extends Model
@@ -14,4 +15,13 @@ abstract class AccountingModel extends Model
         LogsAccountingActivity::getActivitylogOptions insteadof LogsActivity;
     }
     use LogsActivity;
+
+    public function getTable(): string
+    {
+        if (isset($this->table)) {
+            return $this->table;
+        }
+
+        return 'accounting_' . Str::snake(Str::pluralStudly(class_basename($this)));
+    }
 }
